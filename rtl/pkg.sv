316fb39c833595a8700ba29df778a0fb3b8888ec
Oninelol <Oninelol@outlook.com>
2026-06-16 04:10:48 +0800

    current work

---
+++ rtl/pkg.sv
@@ -0,0 +1,12 @@
+package pkg 
+
+    typedef enum logic [6:0]{
+        ALU_R = 7'b0110011;
+        ALU_I = 7'b0010011;
+        LOAD =  7'b0000011;
+        STORE = 7'b1100011;
+        BRANCH = 
+    } opcode;
+
+
+endpackage 
