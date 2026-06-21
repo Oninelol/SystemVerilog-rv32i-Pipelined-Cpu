ab08594bd26718c7392883548af24ec517704e66
Oninelol <Oninelol@outlook.com>
2026-06-21 16:04:12 +0800

    added ALU control logic and mem_write mem_read logic

--- rtl/control.sv
+++ rtl/control.sv
@@ -3,13 +3,30 @@ import pkg::*;
 module control_unit{
     input [31:0] instr;
     output logic reg_write;
-    output [] alu_sel;
+    output [1:0] alu_sel;
     output logic mem_read;
     output logic mem_write;
     output logic branch_code;
 }
 
+    always_comb begin // logic for ALU control signals, determining operation type
+        case(instr[6:0])
+            ALU_R: alu_sel = 2'b00;
+            ALU_I: alu_sel = 2'b01;
+            (STORE || LOAD): alu_sel = 2'b10;
+            BRANCH: alu_sel = 2'b11;
+            default: alu_sel = 2'b11; // assign ALU control code to 2'b11 as default, so the default is ADD just like in branch.
+        endcase // default as 2'b11 is high, convinient for debugging purposes
+    end
 
+    always_comb begin 
+        mem_read = 0;
+        mem_write = 0;
+        case(instr[6:0])
+            STORE: mem_write = 1;
+            LOAD: mem_read = 1;
+        endcase
+    end
 
 
 endmodule
