ef439331a0387c97b48020c31f7cef0471edbcc2
Oninelol <Oninelol@outlook.com>
2026-06-22 19:09:32 +0800

    fixes to control and layout of regfile done

--- rtl/control.sv
+++ rtl/control.sv
@@ -22,7 +22,7 @@ module control_unit{
 
     always_comb begin // logic for reg_write
         reg_write = 0;
-        if(instr[6:0] == ALU_R || instr[6:0] == ALU_I || instr[6:0] == STORE || instr[6:0] == JAL || instr[6:0] == JAL_R || instr[6:0] == LUI || instr[6:0] == AUIPC) begin
+        if(instr[6:0] == ALU_R || instr[6:0] == ALU_I || instr[6:0] == LOAD || instr[6:0] == JAL || instr[6:0] == JAL_R || instr[6:0] == LUI || instr[6:0] == AUIPC) begin
             reg_write = 1;
         end
     end
--- rtl/regfile.sv
+++ rtl/regfile.sv
@@ -1,13 +1,21 @@
 import pkg::*;
 
 module register_file{
+    input clk;
+    input rst;
     input logic reg_write;
-    input [4:0] read_reg1;
-    input [4:0] read_reg2;
-    input [4:0] write_reg;
-    input [31:0] write_data;
+    input [4:0] rs1;
     output [31:0] read_data1;
+    input [4:0] rs2;
     output [31:0] read_data2;
+    input [4:0] rd;
+    input [31:0] write_data;
 }
+
+    [31:0] regs [31:0]; // 32 Registers that are each 32 bit wide in regfile
+
+    always_ff @(posedge clk) begin
+
+    end
 
 endmodule
