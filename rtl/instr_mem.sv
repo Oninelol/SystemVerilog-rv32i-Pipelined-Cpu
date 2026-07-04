049f821d6a15596540898030dfda050ed3563990
Oninelol <Oninelol@outlook.com>
2026-07-04 20:07:46 +0900

    instruction memory written

--- rtl/instr_mem.sv
+++ rtl/instr_mem.sv
@@ -2,8 +2,12 @@ import pkg::*;
 
 module instruction_memory{
     input [31:0] addr;
-    input clk;
     output [31:0] instr;
 }
+
+    logic [31:0] mem[1023:0]; // 4KB insruction memory 
+    initial $readmemh("test_program.hex",mem); // load the hex program (assembly) into instruction memory
+    assign instr = mem[addr[11:2]]; // drop addr[1:0] to divide 4, as mem address indexes incrementes by 1, while pc is incremented by 4
+    // drop addr[31:12] as memory is not large enough to use them
 
 endmodule
