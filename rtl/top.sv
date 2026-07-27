a06c390028353e3a80cba8b828c5dd2b19c1143f
Oninelol <Oninelol@outlook.com>
2026-07-28 00:29:58 +0800

    pc logic completed

--- rtl/top.sv
+++ rtl/top.sv
@@ -20,8 +20,43 @@ module top(
     logic RegWrite;
     logic [1:0] ALUOp;
     logic MemRead,MemWrite,BranchEnable,ALUSrc,MemtoReg;
-    logic zero,negative;
+    logic zero,lt_signed,lt_unsigned; 
     logic jump;
+
+    // pc logic below
+    logic branch_taken; // logic signal indicating if a branch is taken 
+    wire [31:0] pc_plus_4, pc_target;
+    assign pc_plus_4 = read_addr_wire + 32'd4; // default pc incrementation logic
+
+    always_comb begin
+        case(instr[14:12])
+            3'b000: branch_taken = zero; // branch equal
+            3'b001: branch_taken = ~zero; // branch not equal
+            3'b100: branch_taken = lt_signed; // branch less than (signed)
+            3'b101: branch_taken = ~lt_signed; // branch greater or equal to (signed)
+            3'b110: branch_taken = lt_unsigned; // branch less than (unsigned)
+            3'b111: branch_taken = ~lt_unsigned; // branch greater or equal to (unsigned)
+            default: branch_taken = 0;
+        endcase
+    end
+
+    always_comb begin
+        if(jump & instr[6:0] = JALR) begin
+            pc_in = (read_data_1 + imm_result) & ~32'd1; // JALR, clears bit 0
+        end
+        else if(jump) begin
+            pc_in = read_addr_wire + imm_result; // JAL = pc + imm;
+        end
+        else if(BranchEnable && branch_taken) begin 
+            pc_in = read_addr_wire + imm_result; // Branching, pc += imm;
+        end
+        else begin // regular case of incrementing by 4
+            pc_in = pc_plus_4;
+        end
+    end
+
+    assign pc_out = read_addr_wire; // set output wire pc_out to read_addr_wire also to debug
+    assign alu_res = alu_result; // set output wire alu_res = alu_result for debugging purposes
 
     // below connects the modules
     program_counter cpu_pc (
@@ -30,21 +65,6 @@ module top(
         .rst (rst),
         .pc_out (read_addr_wire)
     );  // PC connections
-
-
-    // pc logic below
-
-    logic branch_taken;
-    wire [31:0] pc_plus_4, pc_target;
-
-    assign pc_plus_4 = read_addr_wire + 4; // default pc incrementation logic
-
-    always_comb begin
-        case(instr[14:12])
-            3'b00: 
-        endcase
-    end
-
 
     instruction_memory cpu_imem (
         .addr (read_addr_wire),
@@ -91,7 +111,8 @@ module top(
         .operand_1 (read_data_1),
         .operand_2 (ALUSrc ? imm_result : read_data_2),
         .zero (zero),
-        .negative (negative),
+        .lt_signed (lt_signed),
+        .lt_unsigned(lt_unsigned),
         .alu_result (ALU_result)
     ); // Arithmetic logical unit connections
 
