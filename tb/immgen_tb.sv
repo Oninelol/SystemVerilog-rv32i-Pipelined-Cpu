0db45abf92ae06ae74cd9b53299e696a9099e710
Oninelol <Oninelol@outlook.com>
2026-07-29 14:08:32 +0800

    added testing task for immgen module

--- tb/immgen_tb.sv
+++ tb/immgen_tb.sv
@@ -3,5 +3,32 @@ import pkg::*;
 
 module immgen_tb;
 
+    logic [31:0] instr;
+    logic [31:0] imm;
+
+    imm_generator DUT( // instantiate device under test (immgen module)
+        .instr (instr),
+        .imm (imm)
+    );
+
+    integer errors = 0;
+    integer tests = 0;
+
+    task check_imm( // create task to compare generated imm to expected value
+        input [31:0] input_instr,
+        input [31:0] expected_imm,
+        input string name
+    );
+    instr = input_instr;
+    #1;
+    tests = tests + 1;
+    if(imm != expected_imm) begin
+        errors = errors + 1;
+        $display("FAILED %s TEST, imm=%08h expected=%08h",name,imm,expected_imm);
+    end
+    endtask
+
+    
+
 
 endmodule
