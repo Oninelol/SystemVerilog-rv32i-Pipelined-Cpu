04c6d08e24b1b0c437e7df07d34e1e04f5502a22
Oninelol <Oninelol@outlook.com>
2026-07-29 16:21:05 +0800

    added helper function to display operation name

--- tb/alu_control_tb.sv
+++ tb/alu_control_tb.sv
@@ -18,10 +18,47 @@ module alu_control_tb;
     integer errors = 0;
     integer tests = 0;
 
-    task check_alu_op(
+    function string op_name(input [4:0] op_) // helper function to display result and expected operations with names
+        case(op_)
+            ALU_ADD: return "ADD";
+            ALU_SUB: return "SUB";
+            ALU_XOR:  return "XOR";
+            ALU_OR:   return "OR";
+            ALU_AND:  return "AND";
+            ALU_SLL:  return "SLL";
+            ALU_SRL:  return "SRL";
+            ALU_SRA:  return "SRA";
+            ALU_SLT:  return "SLT";
+            ALU_SLTU: return "SLTU";
+            ALU_MUL:  return "MUL";
+            ALU_MULH: return "MULH";
+            ALU_MULU: return "MULU";
+            ALU_DIV: return "DIV";
+            ALU_DIVU: return "DIVU";
+            ALU_REM: return "REM";
+            ALU_REMU: return "REMU";
+            default: return $sformatf("UNKNOWN OPCODE %05b",op_);
+        endcase
+    endfunction
 
+    task check_alu_op( // task to check if alu operations match what is expected
+        input [1:0] input_alu_type,
+        input [31:0] input_instr,
+        input [4:0] expected_alu_op,
+        input string name
     );
-
+    alu_type = input_alu_type;
+    instr = input_instr;
+    #1;
+    tests = tests + 1;
+    if(alu_op !== expected_alu_op) begin
+        errors = errors + 1;
+        $display("FAILED %s TEST, ");
+    end
     endtask
+
+    initial begin
+        
+    end
 
 endmodule
