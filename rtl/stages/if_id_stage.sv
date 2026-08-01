45539773099c93aa558fb5e5f9748f4e0a836406
Oninelol <Oninelol@outlook.com>
2026-08-02 03:19:41 +0800

    pipeline stage registers

---
+++ rtl/stages/if_id_stage.sv
@@ -0,0 +1,13 @@
+module if_id_reg(
+    input clk,
+    input rst,
+    input stall,
+    input flush,
+    input [31:0] pc_plus4_in,
+    input [31:0] instr_in,
+    output [31:0] pc_plus4_out,
+    output [31:0] instr_out
+);
+
+
+endmodule
