1f8cfe2235ddb9d76aa3171b596daadea649197f
Oninelol <Oninelol@outlook.com>
2026-08-02 14:40:42 +0800

    state register fetch->decode

--- rtl/stages/if_id_stage.sv
+++ rtl/stages/if_id_stage.sv
@@ -1,13 +1,24 @@
 module if_id_reg(
-    input clk,
-    input rst,
-    input stall,
-    input flush,
-    input [31:0] pc_plus4_in,
-    input [31:0] instr_in,
-    output [31:0] pc_plus4_out,
-    output [31:0] instr_out
+    input logic clk,
+    input logic rst,
+    input logic stall,
+    input logic flush,
+    input logic [31:0] pc_in, // pc_plus_4 connects in
+    input logic [31:0] instr_in,
+    output logic [31:0] pc_out, 
+    output logic [31:0] instr_out
 );
 
+    always_ff @(posedge clk) begin
+        if(rst || flush) begin // when reset or flush triggered
+            pc_out <= 32'd0; // reset PC
+            instr_out <= 32'h00000013; // addi x0,x0,0;
+        end
+        else if(~stall) begin // if no stall, move forward
+            pc_out <= pc_in;
+            instr_out <= instr_in;
+        end 
+        // when stall is triggered, the contents in register is held for the clock cycle
+    end
 
 endmodule
