dfdf0b8f868911b034b5cee2e5ce3f9281d099d6
Oninelol <Oninelol@outlook.com>
2026-08-02 21:47:09 +0800

    modified ex->mem register skeleton

--- rtl/stages/ex_mem_stage.sv
+++ rtl/stages/ex_mem_stage.sv
@@ -1,28 +1,26 @@
 module ex_mem_reg(
+    input logic clk,
+    input logic rst,
     input logic [31:0] pc_result_in,
     input logic [31:0] alu_result_in,
     input logic [31:0] write_data_in,
     input logic [4:0] rd_in,
+    input logic [2:0] funct3_in,
     input logic reg_write_in,
     input logic mem_read_in,
     input logic mem_write_in,
-    input logic branch_enable_in,
     input logic mem_to_reg_in,
-    input logic zero_in,
-    input logic lt_signed_in,
-    input logic lt_unsigned_in,
+    input logic jump_in,
     output logic [31:0] pc_result_out,
     output logic [31:0] alu_result_out,
     output logic [31:0] write_data_out,
     output logic [4:0] rd_out,
+    output logic [2:0] funct3_out,
     output logic reg_write_out,
     output logic mem_read_out,
     output logic mem_write_out,
-    output logic branch_enable_in,
     output logic mem_to_reg_out,
-    output logic zero_out,
-    output logic lt_signed_out, 
-    output logic lt_unsigned_out
+    output logic jump_out
 );
 
 
