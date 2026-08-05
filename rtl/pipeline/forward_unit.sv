38b6dbbff28db5ef49734ecef08d341077bd984e
Oninelol <Oninelol@outlook.com>
2026-08-05 16:04:14 +0800

    forwarding unit skeleton

---
+++ rtl/pipeline/forward_unit.sv
@@ -0,0 +1,14 @@
+module forwarding_unit(
+    input logic [4:0] rs,
+    input logic [4:0] rt,
+    input logic [4:0] ex_mem_rd,
+    input logic [4:0] mem_wb_rd,
+    input logic ex_mem_regwrite_in,
+    input logic mem_wb_regwrite_in,
+    output logic [1:0] forward_A,
+    output logic [1:0] forward_B
+);
+
+
+
+endmodule
