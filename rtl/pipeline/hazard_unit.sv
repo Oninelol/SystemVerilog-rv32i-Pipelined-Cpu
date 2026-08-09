b52913d24c23ab8de22dcce5378b17518bbd220e
Oninelol <Oninelol@outlook.com>
2026-08-09 15:34:35 +0800

    hazard unit

--- rtl/pipeline/hazard_unit.sv
+++ rtl/pipeline/hazard_unit.sv
@@ -11,4 +11,11 @@ module hazard_unit(
     output logic id_ex_flush
 );
 
+    logic load_use; // create load_use logic that is true when the n+1 instruction uses the register loaded by instruction n
+    assign load_use = (id_ex_memread) && (id_ex_rd != 5'b00000) && ((if_id_rs == id_ex_rd) || (if_id_rt == id_ex_rd)); // discard load into reg x0
+    assign pc_stall = load_use;
+    assign if_id_stall = load_use; 
+    assign if_id_flush = (branch_taken || jump); 
+    assign id_ex_flush = (load_use || branch_taken || jump); 
+
 endmodule
