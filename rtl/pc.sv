0e51e3b05693661717219958b6be58836d267035
Oninelol <Oninelol@outlook.com>
2026-08-18 23:33:51 +0800

    pc (with stalling)

--- rtl/pc.sv
+++ rtl/pc.sv
@@ -4,6 +4,7 @@ import pkg::*;
     input [31:0] pc_in,
     input logic clk,
     input logic rst,
+    input logic stall,
     output logic [31:0] pc_out
 );
 
@@ -11,8 +12,8 @@ import pkg::*;
         if(rst) begin
             pc_out <= 32'd0; // reset pc to 0 when reset signal
         end
-        else begin
-            pc_out <= pc_in;    // otherwise, output the pc input to perform operations
+        else if (~stall) begin 
+            pc_out <= pc_in;    // if pc not stalled and not reset, output the pc input to perform operations
         end
     end   
 
